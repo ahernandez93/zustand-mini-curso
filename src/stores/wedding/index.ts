acaaa0cac0ae47b41cc180528c3cdbdacb82f878
Allan <allan_hc@outlook.com>
2026-05-13 18:06:58 -0600

    siguiendo el curso, hasta video 58

---
+++ src/stores/wedding/index.ts
@@ -0,0 +1,15 @@
+import { create } from "zustand";
+import { createPersonSlice, PersonSlice } from "./person.slice";
+import { devtools } from "zustand/middleware";
+import { createGuestSlice, GuestSlice } from "./guest.slice";
+import { createDateSlice, DateSlice } from "./date.slice";
+
+type ShareState = PersonSlice & GuestSlice & DateSlice;
+
+export const useWeddingBoundStore = create<ShareState>()(
+  devtools((...a) => ({
+    ...createPersonSlice(...a),
+    ...createGuestSlice(...a),
+    ...createDateSlice(...a),
+  })),
+);
